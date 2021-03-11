4d0fa1c92f5c375c5126dc483adf23cd5d227768
gussmith23 <guscomps@gmail.com>
2021-03-11 23:19:14 +0000

    Deploying to gh-pages from @ gussmith23/glenside-web-demo@c358bb5907f92b74fc4291dba85281f02416515f 🚀

--- pkg/README.md
+++ pkg/README.md
@@ -1,1 +1,6 @@
-Thanks to @mwillsey and his [egg-web-demo repo](https://github.com/egraphs-good/egg-web-demo)!
+# Glenside Web Demo
+
+Thanks to 
+  @mwillsey 
+  and his 
+  [egg-web-demo repo](https://github.com/egraphs-good/egg-web-demo)!
--- pkg/wasm.d.ts
+++ pkg/wasm.d.ts
@@ -1,10 +1,20 @@
 /* tslint:disable */
 /* eslint-disable */
+/**
+*/
+export function run_app(): void;
 
 export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
+  readonly run_app: () => void;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hd5d37ba69ebc2de8: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __wbindgen_start: () => void;
 }
 
 /**
